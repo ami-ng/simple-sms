bdad8b7b3956a7e2bf3394b1debc58db735b2f7b
wessware <stevenshisia@yahoo.com>
2024-04-01 14:54:08 +0300

    login added

--- app/router/index.js
+++ app/router/index.js
@@ -3,7 +3,7 @@ const {Router} = require("express");
 const MainRouter = Router();
 
 MainRouter.get("/", (req, res) => {
-    res.render("index");
+    res.render("pages/index");
 });
 
 module.exports = MainRouter;
