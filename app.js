ed3810e487e2da7ffefdbb4a89fa7af774641800
Surprise Awofemi <114243823+surpriseawofemi@users.noreply.github.com>
2024-04-01 14:31:26 +0300

    structure added

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+
 
 const server = app.listen("3000", async () => {
   console.log("Server is running on port 3000");
