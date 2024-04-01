f72d5d8fda60e205222b2f1c13480c39e6c86702
Surprise Awofemi <114243823+surpriseawofemi@users.noreply.github.com>
2024-04-01 14:59:01 +0300

    router login

--- app/router/index.js
+++ app/router/index.js
@@ -1,9 +1,12 @@
 const {Router} = require("express");
+const FrontendRouter = require("./frontend");
+const ApiRouter = require("./api");
 
 const MainRouter = Router();
 
-MainRouter.get("/", (req, res) => {
-    res.render("pages/index");
-});
+MainRouter.use("/", FrontendRouter);
+MainRouter.use("/api", ApiRouter);
+
+
 
 module.exports = MainRouter;
